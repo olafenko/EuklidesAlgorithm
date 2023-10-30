cb316b0290217bf3ace231ca9ce9f0048247d326
olafenko <olopikolo666@gmail.com>
2023-10-30 15:17:06 +0100

    exception catch

--- src/main/java/olafenko/Euklides.java
+++ src/main/java/olafenko/Euklides.java
@@ -1,33 +1,42 @@
 package olafenko;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Euklides {
 
 
-
-
-
-    public void run(){
+    public void run() {
         Scanner scanner = new Scanner(System.in);
 
         EuklidesAlgorithm(scanner);
 
 
-
     }
 
 
-
-    public void EuklidesAlgorithm(Scanner scanner){
-
-        System.out.println("Enter two numbers which greatest common divisor u want to get: ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
+    public void EuklidesAlgorithm(Scanner scanner) {
 
         ArrayList<Integer> commonDivisor = new ArrayList<>();
+
+        int a;
+        int b;
+
+        while (true) {
+            try {
+                System.out.println("Enter two numbers which greatest common divisor u want to get: ");
+
+                a = Integer.parseInt(scanner.nextLine());
+                b = Integer.parseInt(scanner.nextLine());
+
+                break;
+
+            } catch (NumberFormatException nfe) {
+                System.out.println("You must enter a number.");
+
+            }
+        }
 
         int searchRange;
 
@@ -37,9 +46,9 @@ public class Euklides {
             searchRange = a;
         }
 
-        for (int i = 1; i<= searchRange; i++){
+        for (int i = 1; i <= searchRange; i++) {
 
-            if (a%i == 0 && b%i ==0){
+            if (a % i == 0 && b % i == 0) {
                 commonDivisor.add(i);
             }
 
@@ -48,9 +57,11 @@ public class Euklides {
         Integer greatestCommonDivisor = commonDivisor.get(commonDivisor.size() - 1);
 
         System.out.println("Biggest common divider of " + a + " and " + b + " is: " + greatestCommonDivisor);
-
     }
 
 
-
 }
+
+
+
+
