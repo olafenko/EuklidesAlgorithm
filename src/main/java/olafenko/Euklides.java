7c87c99907c4a4c1650930ae0ce7c22cf137b73c
olafenko <olopikolo666@gmail.com>
2023-10-30 11:59:47 +0100

    Initial commit

---
+++ src/main/java/olafenko/Euklides.java
@@ -0,0 +1,56 @@
+package olafenko;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Euklides {
+
+
+
+
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+
+        EuklidesAlgorithm(scanner);
+
+
+
+    }
+
+
+
+    public void EuklidesAlgorithm(Scanner scanner){
+
+        System.out.println("Enter two numbers which greatest common divisor u want to get: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        ArrayList<Integer> commonDivisor = new ArrayList<>();
+
+        int searchRange;
+
+        if (a > b) {
+            searchRange = a;
+        } else {
+            searchRange = b;
+        }
+
+        for (int i = 1; i<= searchRange; i++){
+
+            if (a%i == 0 && b%i ==0){
+                commonDivisor.add(i);
+            }
+
+        }
+
+        Integer greatestCommonDivisor = commonDivisor.get(commonDivisor.size() - 1);
+
+        System.out.println("Biggest common divider of " + a + " and " + b + " is: " + greatestCommonDivisor);
+
+    }
+
+
+
+}
